85cf51ce388683cafd09db06ccc410c226836b6b
michaelflops <mwbetts@ucsc.edu>
2016-07-26 21:35:40 -0700

    added gif

--- DayNnite.js
+++ DayNnite.js
@@ -1,10 +1,14 @@
-(function(){
-  var now = new Date();
-  now.getHours();
-  console.log(now);
-  if(now >= 18 && now <= 6){
+(function($){
+  var now = new Date().getHours();
+  now = 12;
+  if(now >= 18 || now <= 6){
     // load night.css at the end of the head tag
-
+    // $('head').append('<link href="night-bootstrap.min.css" rel="stylesheet">');
+    $("<link/>", {
+       rel: "stylesheet",
+       type: "text/css",
+       href: "night-bootstrap.min.css"
+    }).appendTo("head");
   }
 
-}());
+}(jQuery));
